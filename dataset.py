75223a377b4fdfbeb51e5f1b10f2a3ca38484841
KactusJec <1318508185@qq.com>
2024-11-25 23:06:30 +0800

    Dataloader

--- dataset.py
+++ dataset.py
@@ -14,7 +14,7 @@ from recbole.utils import (
 )
 from myutils import *
 from recbole.data.dataloader.abstract_dataloader import (
-    AbstractDataLoader,NegSampleDataLoader
+    AbstractDataLoader
 )
 from recbole.utils import InputType, FeatureType, FeatureSource
 from numpy.random import sample
@@ -226,6 +226,129 @@ class FourSquare(SequentialDataset):
     def __getitem__(self, idx):
         return self.inter_feat.iloc[idx] 
 
+class NegSampleDataLoader(AbstractDataLoader):
+    """:class:`NegSampleDataLoader` is an abstract class which can sample negative examples by ratio.
+    It has two neg-sampling method, the one is 1-by-1 neg-sampling (pair wise),
+    and the other is 1-by-multi neg-sampling (point wise).
+
+    Args:
+        config (Config): The config of dataloader.
+        dataset (Dataset): The dataset of dataloader.
+        sampler (Sampler): The sampler of dataloader.
+        shuffle (bool, optional): Whether the dataloader will be shuffle after a round. Defaults to ``False``.
+    """
+
+    def __init__(self, config, dataset, sampler, shuffle=True):
+        self.logger = getLogger()
+        super().__init__(config, dataset, sampler, shuffle=shuffle)
+
+    def _set_neg_sample_args(self, config, dataset, dl_format, neg_sample_args):
+        self.uid_field = dataset.uid_field
+        self.iid_field = dataset.iid_field
+        self.dl_format = dl_format
+        self.neg_sample_args = neg_sample_args
+        self.times = 1
+        if (
+            self.neg_sample_args["distribution"] in ["uniform", "popularity"]
+            and self.neg_sample_args["sample_num"] != "none"
+        ):
+            self.neg_sample_num = self.neg_sample_args["sample_num"]
+
+            if self.dl_format == InputType.POINTWISE:
+                self.times = 1 + self.neg_sample_num
+                self.sampling_func = self._neg_sample_by_point_wise_sampling
+
+                self.label_field = config["LABEL_FIELD"]
+                dataset.set_field_property(
+                    self.label_field, FeatureType.FLOAT, FeatureSource.INTERACTION, 1
+                )
+            elif self.dl_format == InputType.PAIRWISE:
+                self.times = self.neg_sample_num
+                self.sampling_func = self._neg_sample_by_pair_wise_sampling
+                self.neg_prefix = config["NEG_PREFIX"]
+                self.neg_item_id = self.neg_prefix + self.iid_field
+
+                columns = (
+                    [self.iid_field]
+                    if dataset.item_feat is None
+                    else dataset.item_feat.columns
+                )
+                for item_feat_col in columns:
+                    neg_item_feat_col = self.neg_prefix + item_feat_col
+                    dataset.copy_field_property(neg_item_feat_col, item_feat_col)
+            else:
+                raise ValueError(
+                    f"`neg sampling by` with dl_format [{self.dl_format}] not been implemented."
+                )
+
+        elif (
+            self.neg_sample_args["distribution"] != "none"
+            and self.neg_sample_args["sample_num"] != "none"
+        ):
+            raise ValueError(
+                f'`neg_sample_args` [{self.neg_sample_args["distribution"]}] is not supported!'
+            )
+
+    def _neg_sampling(self, inter_feat):
+        if self.neg_sample_args.get("dynamic", False):
+            candidate_num = self.neg_sample_args["candidate_num"]
+            user_ids = inter_feat[self.uid_field].to_numpy()
+            item_ids = inter_feat[self.iid_field].to_numpy()
+            neg_candidate_ids = self._sampler.sample_by_user_ids(
+                user_ids, item_ids, self.neg_sample_num * candidate_num
+            )
+            self.model.eval()
+            interaction = copy.deepcopy(inter_feat).to(self.model.device)
+            interaction = interaction.repeat(self.neg_sample_num * candidate_num)
+            neg_item_feat = Interaction(
+                {self.iid_field: neg_candidate_ids.to(self.model.device)}
+            )
+            interaction.update(neg_item_feat)
+            scores = self.model.predict(interaction).reshape(candidate_num, -1)
+            indices = torch.max(scores, dim=0)[1].detach()
+            neg_candidate_ids = neg_candidate_ids.reshape(candidate_num, -1)
+            neg_item_ids = neg_candidate_ids[
+                indices, [i for i in range(neg_candidate_ids.shape[1])]
+            ].view(-1)
+            self.model.train()
+            return self.sampling_func(inter_feat, neg_item_ids)
+        elif (
+            self.neg_sample_args["distribution"] != "none"
+            and self.neg_sample_args["sample_num"] != "none"
+        ):
+            user_ids = inter_feat[self.uid_field].to_numpy()
+            item_ids = inter_feat[self.iid_field].to_numpy()
+            neg_item_ids = self._sampler.sample_by_user_ids(
+                user_ids, item_ids, self.neg_sample_num
+            )
+            return self.sampling_func(inter_feat, neg_item_ids)
+        else:
+            return inter_feat
+
+    def _neg_sample_by_pair_wise_sampling(self, inter_feat, neg_item_ids):
+        inter_feat = inter_feat.repeat(self.times)
+        neg_item_feat = Interaction({self.iid_field: neg_item_ids})
+        neg_item_feat = self._dataset.join(neg_item_feat)
+        neg_item_feat.add_prefix(self.neg_prefix)
+        inter_feat.update(neg_item_feat)
+        return inter_feat
+
+    def _neg_sample_by_point_wise_sampling(self, inter_feat, neg_item_ids):
+        pos_inter_num = len(inter_feat)
+        new_data = inter_feat.repeat(self.times)
+        new_data[self.iid_field][pos_inter_num:] = neg_item_ids
+        new_data = self._dataset.join(new_data)
+        labels = torch.zeros(pos_inter_num * self.times)
+        labels[:pos_inter_num] = 1.0
+        new_data.update(Interaction({self.label_field: labels}))
+        return new_data
+
+    def get_model(self, model):
+        self.model = model
+
+
+
+
 class MyTrainDataLoader(NegSampleDataLoader):
     def __init__(self, config, dataset, sampler, shuffle):
         self.sample_size = len(dataset)
@@ -238,8 +361,12 @@ class MyTrainDataLoader(NegSampleDataLoader):
         self.step = self.batch_size 
 
     def collate_fn(self, index):
-        return self._neg_sampling(self._dataset.inter_feat.iloc[index])
-    
+        index = np.array(index)
+        data = self._dataset[index]
+        transformed_data = self.transform(self._dataset, data)
+        return self._neg_sampling(transformed_data)
+
+
     def _neg_sampling(self, inter_feat):
             if self.neg_sample_args.get("dynamic", False):
                 candidate_num = self.neg_sample_args["candidate_num"]
